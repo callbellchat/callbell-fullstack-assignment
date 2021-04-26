1de7fc9a6f69a8d266cf99f5ccbc3f82bf41073c
Gianluca Bargelli <gianluca@callbell.eu>
2021-04-26 19:01:41 +0200

    Update card_spec.rb

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
+  # TODO: Test your card model logic here (if present).
   pending "add some examples to (or delete) #{__FILE__}"
 end
