abf75a8dd6cdd731763cc4caa101cf7906b4e011
michal.konopka <michal.konopka@intive.com>
2022-12-22 21:18:48 +0000

    feat: add first fields to cards table

--- db/migrate/20221222163446_create_cards.rb
+++ db/migrate/20221222163446_create_cards.rb
@@ -1,6 +1,8 @@
 class CreateCards < ActiveRecord::Migration[6.1]
   def change
     create_table :cards do |t|
+      t.string :name
+      t.text :desc
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_150825) do
+ActiveRecord::Schema.define(version: 2022_12_22_163446) do
 
   create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
