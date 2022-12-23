d7c46f97e19427626c5d53d5e569bde906813f76
michal.konopka <michal.konopka@intive.com>
2022-12-23 00:36:11 +0000

    feat: add ruby-trello to interact with Trello API

---
+++ config/initializers/trello.rb
@@ -0,0 +1,8 @@
+require 'trello'
+
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_KEY']
+  config.member_token = ENV['TRELLO_TOKEN']
+
+  config.http_client = 'faraday'
+end
