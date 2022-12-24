2e579b27e8203c35030f7937cec76345c34e9bbb
michal.konopka <michal.konopka@intive.com>
2022-12-24 13:08:31 +0000

    feat: add card model tests

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  # TODO: Test your card model logic here (if present).
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Card.new(name: 'any name',
+             list_id: '321',
+             remote_trello_card_id: '123')
+  end
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a list_id' do
+    subject.list_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a remote_trello_card_id' do
+    subject.remote_trello_card_id = nil
+    expect(subject).to_not be_valid
+  end
 end
